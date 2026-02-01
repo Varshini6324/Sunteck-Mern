77b6fb9226ef7f9a4493a3280dbc2192a03dfb6b
Varshini6324 <varshini6324@gmail.com>
2026-02-01 19:32:08 +0530

    Force added new folder

---
+++ BANKEND-DEMO-3/README.md
@@ -0,0 +1,15 @@
+1.Generate package.json 
+    npm init -y
+
+
+2.Create server.js
+
+3. Install ,import "express" and create HTTP server Assign port
+
+
+### Connect MongoDB database 
+
+        1. install mongoose and connect to mongodb server
+        2. create schema of the resourse
+        3. Create Model of the Schema
+        4. Perform DB operations on that Model
---
+++ BANKEND-DEMO-3/server.js
@@ -0,0 +1,32 @@
+import exp from 'express'
+import {userApp} from './APIs/UserAPI.js'
+import {productApp} from './APIs/ProductAPI.js'
+import {connect} from 'mongoose'
+const app=exp()
+const port=4000
+//body parser middleware
+app.use(exp.json())
+//connect to db server
+async function connectDB(){
+    try{
+    await connect('mongodb://localhost:27017/anuragdb2')
+    console.log("DB connection success")
+    //Assign port
+    app.listen(port,()=>console.log("server listening to port 4000..."))
+    }catch(err){
+        console.log("Err in DB connection:",err)
+    }
+}
+
+connectDB()
+//if path starts with user-api forward req to userapi
+app.use('/user-api',userApp)
+app.use('/product-api',productApp)
+
+//error handling middleware
+//function errorHandler(err,req,res,next){
+   // res.json({message:"error",reason:err.message})
+//}
+app.use((err,req,res,next)=>{
+    res.status(500).json({message:"error",reason:err.message})
+})
